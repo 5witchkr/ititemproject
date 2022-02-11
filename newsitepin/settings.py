b163717b87aaab87e56b6b87cd895b042da5093f
5witchkr <95848796+5witchkr@users.noreply.github.com>
2022-02-11 14:57:07 +0900

    Update settings.py

--- newsitepin/settings.py
+++ newsitepin/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-_u&lsa)k)kn@_u6epjhz10-#eoo&%^p#1ja!f@edr1_xr97bvr')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
